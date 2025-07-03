2332832c817dab8558ec85bec9d52c6d93ff0c5c
Mahdi <86263611+Mhdph@users.noreply.github.com>
2025-07-03 20:43:40 +0330

    feat:add chi package

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/mhdph/go-start/internal/app"
+	"github.com/mhdph/go-start/internal/routes"
 )
 
 func main() {
@@ -21,11 +22,14 @@ func main() {
 
 	app.Logger.Println("we are runing our app")
 
+	r := routes.SetupRoutes(app)
+
 	server := &http.Server{
 		Addr:         fmt.Sprintf(":%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
+		Handler:      r,
 	}
 
 	err = server.ListenAndServe()
