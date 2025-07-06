f2716269ae0da2027745e683f1b4766c100df937
Mahdi <86263611+Mhdph@users.noreply.github.com>
2025-07-06 13:14:06 +0330

    feat: implement CreateNewToken and DeleteAllTokensForUser methods in PostgresTokenStore for enhanced token management

--- internal/store/tokens.go
+++ internal/store/tokens.go
@@ -19,3 +19,33 @@ type TokenStore interface {
 	Insert(token *tokens.Token) error
 	Create(userID string, scope string, expiry time.Duration) (*tokens.Token, error)
 }
+
+func (t *PostgresTokenStore) CreateNewToken(userID string, scope string, expiry time.Duration) (*tokens.Token, error) {
+	token, err := tokens.GetTokenStore(userID, scope, expiry)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = t.Insert(token)
+
+	return token, err
+}
+
+func (t *PostgresTokenStore) Insert(token *tokens.Token) error {
+	query := `
+		INSERT INTO tokens (id, user_id, scope, expiry)
+		VALUES ($1, $2, $3, $4)
+	`
+	_, err := t.db.Exec(query, token.Hash, token.UserID, token.Scope, token.Expiry)
+	return err
+}
+
+func (t *PostgresTokenStore) DeleteAllTokensForUser(UserID string, scope string) error {
+	query := ` 
+	DELETE FROM tokens 
+	WHERE scope = $1 AND user_id = $2 
+	`
+	_, err := t.db.Exec(query, scope, UserID)
+	return err
+}
